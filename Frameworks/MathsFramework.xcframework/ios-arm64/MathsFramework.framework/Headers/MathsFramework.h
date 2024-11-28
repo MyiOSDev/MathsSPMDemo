bef0857be71b3af86d912045278c17b0ea667357
sumitmakkar <makkarsumit1986@gmail.com>
2024-11-28 07:38:28 +0530

    initial Commit

---
+++ Frameworks/MathsFramework.xcframework/ios-arm64/MathsFramework.framework/Headers/MathsFramework.h
@@ -0,0 +1,18 @@
+//
+//  MathsFramework.h
+//  MathsFramework
+//
+//  Created by Sumit Makkar on 27/11/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MathsFramework.
+FOUNDATION_EXPORT double MathsFrameworkVersionNumber;
+
+//! Project version string for MathsFramework.
+FOUNDATION_EXPORT const unsigned char MathsFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathsFramework/PublicHeader.h>
+
+
